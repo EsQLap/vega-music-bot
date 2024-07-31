164fb7e93d0c6d36fee6934a77c9030129129df8
EsQLapy <esqlapy@gmail.com>
2024-07-31 14:03:59 +0400

    Add auto deploy script

--- src/main/java/ru/esqlapy/Main.java
+++ src/main/java/ru/esqlapy/Main.java
@@ -2,9 +2,13 @@ package ru.esqlapy;
 
 public final class Main {
 
-    private static final String TOKEN = "";
+    private static final String NEED_TOKEN_AS_PROGRAM_ARGUMENT = "You need pass token as first program argument";
 
     public static void main(String[] args) {
-        Bot.register(TOKEN);
+        if (args.length < 1) {
+            throw new RuntimeException(NEED_TOKEN_AS_PROGRAM_ARGUMENT);
+        }
+        String token = args[0];
+        Bot.register(token);
     }
 }
